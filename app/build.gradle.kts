ca9b27155151d81d7977f6e08763574b495b4a17
AbdulKus <73951988+AbdulKus@users.noreply.github.com>
2026-08-15 17:01:09 +0300

    Bump Essential Remap to 0.1.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.abdulkus.essentialremap"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.1.17"
+        versionCode = 19
+        versionName = "0.1.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
