508defc62e33b5ca50e94ffa34b62f1626bb93a7
free_vibrancy <73951988+AbdulKus@users.noreply.github.com>
2026-09-15 19:20:49 +0300

    Bump Essential Remap to 0.1.39

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.abdulkus.essentialremap"
         minSdk = 26
         targetSdk = 36
-        versionCode = 38
-        versionName = "0.1.38"
+        versionCode = 39
+        versionName = "0.1.39"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
