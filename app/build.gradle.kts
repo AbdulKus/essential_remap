6090483a9ecc2c7e08669877ced225db7c9850a6
free_vibrancy <73951988+AbdulKus@users.noreply.github.com>
2026-09-15 17:02:45 +0300

    Release 0.1.37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.abdulkus.essentialremap"
         minSdk = 26
         targetSdk = 36
-        versionCode = 36
-        versionName = "0.1.36"
+        versionCode = 37
+        versionName = "0.1.37"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
