051205a566689f37418b834b99ac61b6e21019f4
free_vibrancy <73951988+AbdulKus@users.noreply.github.com>
2026-09-18 12:51:38 +0300

    Bump app version to 0.1.43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.abdulkus.essentialremap"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "0.1.42"
+        versionCode = 43
+        versionName = "0.1.43"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
